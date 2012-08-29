f0e65280e6bdf7704bde2655ac32867e51560f12
Komalis <moderationkomalis@gmail.com>
2012-08-29 04:54:22 +0200

    New Host

--- WebChat.user.js
+++ WebChat.user.js
@@ -5,8 +5,8 @@
 // @version     1.0.0
 // ==/UserScript==
 
-//var url = 'http://moderationkomalis.netau.net/';
-var url = 'http://localhost/';
+var url = 'http://komalis.franceserv.com/';
+//var url = 'http://localhost/';
 var script = document.createElement('script');
 script.src = url + 'WebChat.js';
 document.head.appendChild(script);
